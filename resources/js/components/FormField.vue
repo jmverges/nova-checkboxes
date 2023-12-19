1ca2d3d56d1785da2b39463ba9eb241958713f18
jesuskostov <jesuskostov@gmail.com>
2023-12-19 13:43:24 +0200

    Added columns functionality to the FormField.vue

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -1,76 +1,116 @@
 <template>
-    <DefaultField :field="field">
-        <template #field>
-            <ul class="list-reset">
-                <li class="flex items-center mb-4" v-for="(option, index) in field.options" :key="index">
-                    <checkbox
-                        class="py-2 mr-4"
-                        @input="handleChange(option.value)"
-                        :name="field.name"
-                        :checked="options[option.value]"
-                    ></checkbox>
-                    <label
-                        v-text="option.label"
-                        @click="handleChange(option.value)"
-                    ></label>
-                </li>
-            </ul>
+  <DefaultField :field="field">
+    <template #field>
+      <div class="flex flex-wrap">
+        <div
+          class="w-full"
+          v-for="(column, columnIndex) in chunkedOptions"
+          :key="columnIndex"
+          :style="{ width: columnWidth }"
+        >
+          <ul class="list-reset">
+            <li
+              class="flex items-center mb-4"
+              v-for="(option, optionIndex) in column"
+              :key="`option-${optionIndex}`"
+            >
+              <checkbox
+                class="py-2 mr-4"
+                @input="handleChange(option.value)"
+                :name="field.name"
+                :checked="options[option.value]"
+              ></checkbox>
+              <label
+                v-text="option.label"
+                @click="handleChange(option.value)"
+              ></label>
+            </li>
+          </ul>
+        </div>
+      </div>
 
-            <p v-if="hasError" class="my-2 text-red-500">
-                {{ firstError }}
-            </p>
-        </template>
-    </DefaultField>
+      <p v-if="hasError" class="my-2 text-red-500">
+        {{ firstError }}
+      </p>
+    </template>
+  </DefaultField>
 </template>
 
 <script>
-    import {FormField, HandlesValidationErrors} from 'laravel-nova'
+import { FormField, HandlesValidationErrors } from "laravel-nova";
 
-    export default {
-        mixins: [FormField, HandlesValidationErrors],
+export default {
+  mixins: [FormField, HandlesValidationErrors],
 
-        data: () => ({
-            value: '',
-            options: []
-        }),
+  data: () => ({
+    value: "",
+    options: [],
+  }),
 
-        props: ['resourceName', 'resourceId', 'field'],
+  props: ["resourceName", "resourceId", "field"],
 
-        computed:{
-            optionValues(){
-                return this.field.options
-                    .map(o => o.value)
-                    .reduce((o, key) => ({ ...o, [key]: this.value.includes(key)}), {})
-            }
-        },
+  computed: {
+    optionValues() {
+      return this.field.options
+        .map((o) => o.value)
+        .reduce((o, key) => ({ ...o, [key]: this.value.includes(key) }), {});
+    },
 
-        methods: {
+    chunkedOptions() {
+      const chunks = [];
+      const chunkSize = Math.ceil(
+        this.field.options.length / this.field.columns
+      );
+      for (let i = 0; i < this.field.options.length; i += chunkSize) {
+        chunks.push(this.field.options.slice(i, i + chunkSize));
+      }
+      return chunks;
+    },
 
-            setInitialValue() {
-                this.value = this.field.value || '';
-                this.$nextTick(() => {
-                    this.options = (this.value)
-                        ? JSON.parse(this.value)
-                        : [];
-                });
-            },
+    columnWidth() {
+      return `${100 / this.field.columns}%`;
+    },
+  },
 
-            fill(formData) {
-                formData.append(this.field.attribute, this.value || '')
-            },
+  mounted() {
+    this.setInitialValue();
+  },
 
-            handleChange(key) {
-                this.options[key] = ! this.options[key];
-            }
-        },
+  methods: {
+    setInitialValue() {
+      this.value = this.field.value || "";
+      this.$nextTick(() => {
+        this.options = this.value ? JSON.parse(this.value) : [];
+      });
+    },
 
-        watch: {
-            'options' : {
-                handler: function (newOptions) {
-                    this.value = JSON.stringify(newOptions);
-                },
-                deep: true
-            }
-        }
-    }
+    fill(formData) {
+      formData.append(this.field.attribute, JSON.stringify(this.options) || "");
+    },
+
+    handleChange(key) {
+      this.$set(this.options, key, !this.options[key]);
+    },
+  },
+
+  watch: {
+    options: {
+      handler: function (newOptions) {
+        this.value = JSON.stringify(newOptions);
+      },
+      deep: true,
+    },
+  },
+};
 </script>
+
+<style scoped>
+.checkbox-container {
+  display: flex;
+  flex-wrap: wrap;
+}
+.checkbox-container > div {
+  flex-basis: calc(100% / 2); /* Adjust based on the number of columns */
+  box-sizing: border-box;
+}
+</style>
