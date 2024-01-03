27c8d3085b459c857ecbe63293c178035ca08728
jesuskostov <48697426+jesuskostov@users.noreply.github.com>
2024-01-03 17:01:59 +0200

    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,22 +1,30 @@
-let mix = require("laravel-mix");
-let path = require("path");
+let mix = require('laravel-mix')
+let path = require('path')
 
-mix
-  .setPublicPath("dist")
-  .js("resources/js/field.js", "js")
-  .vue({ version: 3 })
-  .sass("resources/sass/field.scss", "css")
-  .webpackConfig({
-    externals: {
-      vue: "Vue",
-    },
-    output: {
-      uniqueName: "fourstacks/nova-checkboxes",
-    },
-  })
-  .alias({
-    "laravel-nova": path.join(
-      __dirname,
-      "../../../vendor/laravel/nova/resources/js/mixins/packages.js"
-    ),
-  });
+mix.setPublicPath('dist')
+    .js('resources/js/field.js', 'js')
+    .vue({ version: 3 })
+    .sass('resources/sass/field.scss', 'css')
+    .webpackConfig({
+        externals: {
+            vue: 'Vue'
+        },
+        output: {
+            uniqueName: 'fourstacks/nova-checkboxes'
+        },
+        resolve: {
+            modules: [path.resolve(__dirname, 'node_modules'), 'node_modules'],
+            alias: {
+                'laravel-nova': path.join(
+                    __dirname,
+                    '../../../vendor/laravel/nova/resources/js/mixins/packages.js'
+                )
+            }
+        }
+    })
+    .alias({
+        'laravel-nova': path.join(
+            __dirname,
+            '../../../vendor/laravel/nova/resources/js/mixins/packages.js'
+        )
+    })
